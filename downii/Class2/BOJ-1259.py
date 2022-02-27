69a4953cd630a3ec10c201b2af62a4caf4d3e136
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-02-27 22:07:05 +0900

    .

---
+++ downii/Class2/BOJ-1259.py
@@ -0,0 +1,16 @@
+'''
+author : kyungmin Kim, keung903@naver.com
+github : https://github.com/daeunkwak/Algori-m
+
+title : 팰린드롬수
+description : 문자열, 구현
+'''
+import sys
+
+num_list=[]
+while(1):
+    num = str(sys.stdin.read())
+    if(num == '0'):
+        break
+    num_list.append(num)
+print(num_list)
