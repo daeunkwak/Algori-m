0e577d184bad6e5657c596d12f0b388ee4743be7
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-02-27 17:32:18 +0900

    50288KM	376ms

--- gangmini/BOJ-2164.py
+++ gangmini/BOJ-2164.py
@@ -6,13 +6,13 @@ title : 카드2
 description : 자료구조, 큐
 '''
 
-N = int(input()) #정수 입력
+N = int(input())
 queue = [i for i in range(N+1)] #0~N 1차원 배열
 front = 0
 rear = N
 while(1):
     front = (front+1)%(N+1)
-    if(front==rear):
+    if(front==rear): # print result
         print(queue[rear])
         break
     front = (front + 1) % (N + 1)
