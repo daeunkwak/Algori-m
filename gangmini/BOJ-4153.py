de04e46cc3439703aecf319e6e63c2031460c56a
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-04 23:52:37 +0900

    삼각형의 원리를 잘 이해할 것 map(), reduce(), lamda 등을 잘 사용하면 여러줄에 걸쳐 작성할 코드를 한 줄에 작성 가능!

--- gangmini/BOJ-4153.py
+++ gangmini/BOJ-4153.py
@@ -6,18 +6,24 @@ title : 직각삼각형
 description : 수학, 기하학, 피타고라스 정리
 '''
 
+## lambda 매개변수 : 표현식
+## map(함수, 리스트)
+## reduce(함수, 시퀀스)
+
 from functools import reduce
 while(1):
     a, b, c = map(int, input().split())
     if(a==0&b==0&c==0):
         break
     else:
-        list = [a,b,c]
-        key = max(list)
-        if(key*key == reduce(lambda x, y: x * y, list[-list.index(key)])):
-            print("Right")
+        list = [a,b,c] #각 변의 값을 리스트에 넣음
+        hypo = max(list) #빗변 원소  추출
+        del list[list.index(hypo)] #빗변 원소 제거
+        legs =  reduce(lambda x, y: x**2 + y**2, list)
+        if(hypo*hypo == legs):
+            print("right")
         else:
-            print("Wrong")
+            print("wrong")
 
 
 
