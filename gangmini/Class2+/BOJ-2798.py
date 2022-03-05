8c3f6909c390f3f1c397d11ec7dd7be713dcdebc
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-05 18:27:43 +0900

    creat BOJ-2798

---
+++ gangmini/Class2+/BOJ-2798.py
@@ -0,0 +1,9 @@
+'''
+author : kyungmin Kim, keung903@naver.com
+github : https://github.com/daeunkwak/Algori-m
+
+title : 블랙잭
+description : 브루트포스 알고리즘 '''
+
+#랜덤으로 N개의 카드 생성(합이 M을 넘지 않는 카드 3장 뽑을 수 있어야 함)
+#
