a8248ba1b6d438aa38e08d3b43908f5edd7e4bb1
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-05 18:05:28 +0900

    별 것도 아닌걸로 뻘짓하지 말자ㅏ 꼼꼼하게 작성할 것! 수식을 더 간단하게 만들 방법 없을까?...

--- gangmini/Class2+/BOJ-10250.py
+++ gangmini/Class2+/BOJ-10250.py
@@ -8,11 +8,12 @@ description : 수학, 구현, 사칙연산
 
 #T개의 테스트셋 입력받음
 #H:층 수 W:층마다 방수 N:손님 순서
-T = int(input()) #테스트의 개수
-#test = list({list(map(int, input().split())) for _ in range(T)})
-#print(test)
-for i in list:
-    H, W, N = map(int, input().split())
-    tu = divmod(N, H) #몫과 나머지를 튜플 형식으로 반환
-    #몫+1 => 호수, 나머지 => 층수
-    print(tu[1]*100+tu[0]+1)
+
+T = int(input())
+test = [list(map(int, input().split())) for _ in range(T)]
+for i in test:
+    tu = divmod(i[2], i[0])  #몫과 나머지를 튜플 형식으로 반환
+    if(tu[1]==0): #나머지가 0이면 꼭대기층 #호수는 몫과 동일
+        print(i[0] * 100 + tu[0])
+    else: #몫+1 => 호수, 나머지 => 층수
+        print(tu[1] * 100 + tu[0] + 1)
