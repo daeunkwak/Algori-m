461e1ab04202cecce331609d75b36b7ed33d8587
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-06 16:33:05 +0900

    create 2798

--- gangmini/Class2+/BOJ-2798.py
+++ gangmini/Class2+/BOJ-2798.py
@@ -6,4 +6,7 @@ title : 블랙잭
 description : 브루트포스 알고리즘 '''
 
 #랜덤으로 N개의 카드 생성(합이 M을 넘지 않는 카드 3장 뽑을 수 있어야 함)
-#
+import random
+N, M = map(int, input().split())
+card = random.sample(range(1,100001),N) #중복X
+print(card)
