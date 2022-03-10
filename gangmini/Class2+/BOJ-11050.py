85c536beaf9a6843a8c511dc4861e5150e0b9472
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-11 00:04:05 +0900

    .

--- gangmini/Class2+/BOJ-11050.py
+++ gangmini/Class2+/BOJ-11050.py
@@ -6,8 +6,11 @@ title : 이항계수1
 description : 수학, 구현, 조합론
 '''
 
-N,K = map(int,input.split())
-result = 0
+N, K = map(int, input().split())
+p,s = 1,1
 for i in range(K):
-    result *= (N-i)/(K-i)
-print(result)
+    p*=(N-i)
+    s*=(K-i)
+    #result *= (N-i)/(K-i)
+print(int(p/s))
+#분모 분자 나누셈을 마지막에 해주고 int형으로 바꾸어 준다
