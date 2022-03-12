e28648ba85a7681a5034b4388603a612e8ad6962
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-13 00:47:42 +0900

    .

--- gangmini/Class2+/BOJ-10814.py
+++ gangmini/Class2+/BOJ-10814.py
@@ -9,13 +9,13 @@ description : 정렬
 
 N = int(input())
 mem = list(input().split() for _ in range(N))
+print(mem)
 #mem.sort(key=lambda x:int(x[0])) #value값->age을 기준으로 정렬
 mem = sorted(mem,key=lambda x:int(x[0])) #int형으로 바꿔줘 숫자를 기준으로 정렬해야 정답
 for j in mem:
     print(j[0],j[1])
 
 #변수명.sort() vs sorted() 함수 차이
-
 
 
 
