e3e239947b3f425d14242193a8a63b939a07bca5
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-12 17:54:47 +0900

    .

--- gangmini/Class2+/BOJ-2609.py
+++ gangmini/Class2+/BOJ-2609.py
@@ -6,20 +6,23 @@ title : 최대공약수 최소공배수
 description : 수학, 정수, 유클리드 호제법 '''
 
 
-def max_(a,b):
-    if(a%b==0):
+def max_f(a,b):
+    if(a % b == 0):
         return b
-    max_(b,a%b)
+    if(a % b == 1):
+        return 1
+    return max_f(b,a%b)
 
 n,m = map(int,input().split())
+#n%m 연산을 위해 n<m인 경우 바꿔줌
 if(n<m):
     tmp = n
     n = m
     m =tmp
-max_div = max_(n,m)
-min_mul = max_div*(n/max_div)*(m/max_div)
-print(max_div) #최대공약수
-print(min_mul) #최소공배수 ##최대공약수*(최대공약수로 각 수 나눈값)
+mmax = max_f(n,m)
+print(mmax) #최대공약수
+mmin = mmax*(n//mmax)*(m//mmax)
+print(mmin) #최소공배수 ##최대공약수*(최대공약수로 각 수 나눈값)
 
 ##유클리드 호제법
 #큰 수를 작은 수로 나눠서 나머지가 나오면 이번엔 작은 수를 나머지로 나눈다.
