d80575b53c92257e89327f71617d3f27f307d3a6
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-20 02:48:22 +0900

    .

--- gangmini/Class2+/BOJ-1018.py
+++ gangmini/Class2+/BOJ-1018.py
@@ -2,6 +2,11 @@
 author : kyungmin Kim, keung903@naver.com
 github : https://github.com/daeunkwak/Algori-m
 
-title :
-description :
+title : 체스판 다시 칠하기
+description : 브루트포스 알고리즘
 '''
+
+while(1):
+    N, M = int(input().split())
+    if(8<=N,M<=50):
+        break
--- gangmini/Class2+/BOJ-11650.py
+++ gangmini/Class2+/BOJ-11650.py
@@ -2,6 +2,7 @@
 author : kyungmin Kim, keung903@naver.com
 github : https://github.com/daeunkwak/Algori-m
 
-title : 
-description :
+title : 좌표 정렬하기
+description : 정렬
 '''
+
