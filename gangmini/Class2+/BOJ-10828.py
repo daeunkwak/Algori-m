394a48b20e70e41df1c22539fce9c50261d0e9c0
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-03-28 00:01:55 +0900

    create 10828

---
+++ gangmini/Class2+/BOJ-10828.py
@@ -0,0 +1,51 @@
+'''
+author : kyungmin Kim, keung903@naver.com
+github : https://github.com/daeunkwak/Algori-m
+
+title : 스택
+description : 자료구조, 스택
+'''
+
+
+stack=[]
+
+#맨 앞에 원소 추가하고 싶으면 insert(인덱스,원소)
+def push(x):
+    stack.append(x)
+
+def pop():
+    if (len(stack) == 0):
+        print(-1)
+        return;
+    print(stack[len(stack)-1])
+    stack.pop(len(stack)-1)
+
+def size():
+    print(len(stack))
+
+def empty():
+    if(len(stack)==0):
+        print(1)
+    else:
+        print(0)
+
+def top():
+    if (len(stack) == 0):
+        print(-1)
+        return;
+    print(stack[len(stack)-1])
+
+
+N = int(input())
+statment = [list(map(str,input().split())) for _ in range(N)]
+for i in statment:
+    if(i[0]=='push'):
+        push(i[1])
+    elif(i[0]=='pop'):
+        pop()
+    elif (i[0] == 'size'):
+        size()
+    elif (i[0] == 'empty'):
+        empty()
+    elif (i[0] == 'top'):
+        top()
--- gangmini/Class2+/BOJ-10866.py
+++ gangmini/Class2+/BOJ-10866.py
@@ -3,15 +3,15 @@ author : kyungmin Kim, keung903@naver.com
 github : https://github.com/daeunkwak/Algori-m
 
 title : 덱
-description : 자료구조, 덱,
+description : 자료구조, 덱
 '''
 
 #덱: 일차원 선형 자료구조, 스택과 큐 연산을 모두 지원하는 양방향 연결리스
 #파이썬에서는 collections 모듈에 deque 클래스로 구현
 
 deque=[]
-#맨 앞에 원소 추가하고 싶으면 insert(인덱스,원소)
 
+#맨 앞에 원소 추가하고 싶으면 insert(인덱스,원소)
 def push_front(x):
     deque.insert(0,x)
 
