07083216b2fa2f2b745f70d5e65eeb7acd6030ce
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-07-11 22:07:36 +0900

    괄호 성공

--- gangmini/Class2+/BOJ-9012.py
+++ gangmini/Class2+/BOJ-9012.py
@@ -9,7 +9,7 @@ description : 자료 구조, 문자열, 스택
 from collections import deque
 N = int(input())
 statment = [list(map(str, input())) for _ in range(N)]
-print(statment)
+#print(statment)
 
 # 조건1.(가 들어오면 넣는다
 # 조건2. )가 들어오면 그 이전 원소가 (면 함께 pop한다. )전에 (가 없으면 NO!
@@ -18,18 +18,16 @@ print(statment)
 for i in statment:
     stack = deque()
     for j in range(len(i)):
-        if(i[j]=='('):
-            stack.append('(')
+        if(i[j]==')' and len(stack)!=0): # 조건2_빈스택일 경우 성립X
+            s = stack.pop()
+            if (s != '('):
+                stack.append(')')
             #print(stack)
-        elif(i[j]==')' and len(stack)!=0):
-            if(stack.pop()!='('):
-                print('NO')
-                break
         else:
-            print('NO')
-            break
-    #print(stack)
+            stack.append(i[j])
+            #print(stack)
 
+    # stack에 남은 요소가 있으면 NO!
     if(len(stack)==0):
         print('YES')
     else:
